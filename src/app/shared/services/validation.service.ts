68cf41e422fc5f8746c780347ebdc4b71e54b339
Bruno Kamada <brunokamada72@gmail.com>
2022-03-21 19:54:16 -0300

    refactor: :recycle: change method option in validation service

--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -7,7 +7,7 @@ export class ValidationService {
   public static getValidationErrorMessage(validatorName: string, validatorValue?: any): any {
     const config: any = {
       required: `Campo é requerido.`,
-      invalidAmount: `O valor a resgatar não pode ser maior que R$ ${ validatorValue.toLocaleString('pt-br', { minimumFractionDigits: 2 }) }.`,
+      invalidAmount: `O valor a resgatar não pode ser maior que R$ ${ validatorValue.toLocaleString('pt-br', { maximumFractionDigits: 2 }) }.`,
     };
 
     return config[validatorName];
